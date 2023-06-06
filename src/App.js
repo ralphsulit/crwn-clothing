9f104679636e1b6f691f03d273771e7730a00151
Joseph <ralphlsulit@gmail.com>
2023-06-06 21:18:20 +0800

    created a sign-in route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home';
+import SignIn from './routes/sign-in/sign-in';
 import Navigation from './routes/navigation/navigation';
 
 const Shop = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
