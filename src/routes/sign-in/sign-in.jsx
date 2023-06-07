c0b04848b66dd1f57bce6c92dc27fd59a167210e
Joseph <ralphlsulit@gmail.com>
2023-06-07 21:39:51 +0800

    updated the sign-in component by adding sign in with google pop up

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,6 +1,18 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase';
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  }
+
   return (
-    <h1>Sign In Page</h1>
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign In with Google
+      </button>
+    </div>
   );
 };
 
