8504dfeef3b9243346ad7d6e2061a20f5bccb2af
Joseph <ralphlsulit@gmail.com>
2023-06-23 22:10:58 +0800

    removed useState in cart-dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,13 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 
 import Button from '../button/button';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
-  const [isActive, setActive] = useState(true);
-
-  const toggleClass = () => {
-    setActive(!isActive);
-  }
-
   return (
     <Fragment>
-      <section
-        className={isActive ? 'cart-dropdown-container' : null}
-        onClick={toggleClass}
-      >
+      <section className='cart-dropdown-container'>
         <div className='cart-items' />
         <Button>GO TO CHECKOUT</Button>
       </section>
