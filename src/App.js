785085c5c9d3177b7fb85fd677a2bf6734d95bbc
Joseph <ralphlsulit@gmail.com>
2023-07-21 22:39:52 +0800

    feat: added checkout page with increasing and decreasing function for product quantity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './routes/home/home';
 import Authentication from './routes/authentication/authentication';
 import Navigation from './routes/navigation/navigation';
 import Shop from './routes/shop/shop';
+import CheckOut from './routes/checkout/checkout.component';
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/auth' element={<Authentication />} />
+        <Route path='/checkout' element={<CheckOut/>} />
       </Route>
     </Routes>
   );
