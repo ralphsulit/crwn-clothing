f1659f34500dee0ccbb402a1ba932fce73a59392
Joseph <ralphlsulit@gmail.com>
2023-08-03 04:37:04 +0800

    feat: added change the db and added in firestore, refactor products context into categories context and used db from firestore

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,22 +1,25 @@
 import { Fragment, useContext } from "react";
 
-import { ProductsContext } from "../../contexts/products.context";
+import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from '../../components/product-card/product-card.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
     <Fragment>
-      <section className="products-container">
-        {products.map((product) => {
-          return (
-            <ProductCard key={product.id} product={product} />
-          )
-        })}
-      </section>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
     </Fragment>
   ); 
 };
