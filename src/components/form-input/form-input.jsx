69d7c5e920fcb3f290c3c0c1e579a733bb30bad1
Joseph <ralphlsulit@gmail.com>
2023-08-15 10:53:15 +0800

    refactor: change form input component style into styled components

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,19 +1,19 @@
-import './form-input.scss';
+import {
+  FormInputLabel,
+  Input,
+  Group
+} from './form-input.style.jsx';
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className='group'>
-    <input className='form-input' {...otherProps} />
+    <Group>
+    <Input {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value.length ? 'shrink' : ''
-          } form-input-label`}
-        >
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 }
 
